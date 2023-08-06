7ca8581f93b25f225ed47369f29f8ec54aa9e1c5
dngocson <dngocson12@gmail.com>
2023-08-06 23:16:03 +0700

    refactor menu component
    refactor menu component

--- src/features/menu/MenuItemList.tsx
+++ src/features/menu/MenuItemList.tsx
@@ -3,103 +3,130 @@ import { useMenu } from "./useMenu";
 import Spinner from "../../ui/Spinner";
 import { MenuItemListProps } from "./type";
 import MenuItem from "../../ui/MenuItem";
+import { menuShortOption1 } from "../../helper/const";
 
 const MenuItemList = () => {
-  const [searchParams] = useSearchParams();
-  const currenntFilter = searchParams.get("type");
-  const type = currenntFilter?.split("_").at(0) || "all";
-  const subType = currenntFilter?.split("_").at(1) || "";
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentFilter = searchParams.get("type");
+  const [type, subType] = currentFilter?.split("_") || ["all", ""];
   const { isLoading, menuItems } = useMenu();
   if (isLoading) return <Spinner />;
   if (!menuItems) return <p>Du lieu tren sever trong</p>;
-  const drinks = menuItems.data.filter((type) => type.type === "drink");
-  const noodles = menuItems.data.filter((type) => type.type === "noodle");
-  const foods = menuItems.data.filter((type) => type.type === "food");
-
-  // const test = drinks.filter((item) => item.sub_type === "drink_tea");
-  // console.log(test);
+  const sortBy = searchParams.get("sortBy") || "name-asc";
+  const [field, direction] = sortBy.split("-");
+  const isAscending = direction === "asc";
+  const sortedMenu = menuItems.data
+    ? [...menuItems.data].sort((a, b) => {
+        const compare =
+          field === "name"
+            ? a[field].localeCompare(b[field])
+            : a[field] - b[field];
+        return isAscending ? compare : -compare;
+      })
+    : [];
+  const sortOptions = menuShortOption1;
+  function handleChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    searchParams.set("sortBy", e.target.value);
+    setSearchParams(searchParams);
+  }
   return (
-    <div className=" mx-8 flex flex-col gap-4">
-      <h1 className="text-2xl font-bold uppercase">
-        {type === "all" ? <p>Menu của Top One</p> : undefined}
-        {(type === "all" || type === "drink") && (
+    <div className=" ml-16 flex flex-col gap-4">
+      <div className="flex flex-col gap-4 text-2xl ">
+        <div className="sticky top-[150px] z-50 flex items-center justify-between">
+          Menu
+          <div>
+            <select
+              className="cursor-pointer rounded-xl p-2 text-base focus:border-none focus:outline-none focus:ring-0"
+              onChange={handleChange}
+            >
+              {sortOptions.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        {["all", "drink"].includes(type) && (
           <SectionPart
             type={type}
-            message="Cà phê, trà sữa Top One"
+            message="Cà phê, trà sữa của Top One"
             subType={subType}
-            data={drinks}
+            data={sortedMenu.filter((item) => item.type === "drink")}
           />
         )}
-        {(type === "all" || type === "noodle") && (
+        {["all", "noodle"].includes(type) && (
           <SectionPart
             type={type}
             message="Mỳ cay của Top One"
             subType={subType}
-            data={noodles}
+            data={sortedMenu.filter((item) => item.type === "noodle")}
           />
         )}
-        {(type === "all" || type === "food") && (
+        {["all", "food"].includes(type) && (
           <SectionPart
             type={type}
             message="Cơm của Top One"
             subType={subType}
-            data={foods}
+            data={sortedMenu.filter((item) => item.type === "food")}
           />
         )}
-      </h1>
+      </div>
     </div>
   );
 };
 
-function SectionPart({
+const SectionPart = ({
   type,
   data,
   subType,
   message: title,
-}: MenuItemListProps) {
-  let message = "";
-  if (type === "drink") {
-    switch (subType) {
-      case "tea":
-        message = "Trà trái cây của Top One";
-        break;
-      case "yogurt":
-        message = "Sữa chua của Top One";
-        break;
-      case "juice":
-        message = "Nước ép, đá xay của Top One";
-        break;
-      case "milk-tea":
-        message = "Trà sữa của Top One";
-        break;
-      case "other":
-        message = "Các thức uống khác của Top One";
-        break;
-      default:
-        message = "Cà phê, trà sữa của Top One";
-    }
-  } else if (type === "noodle") {
-    message = "Mỳ cay của Top One";
-  } else if (type === "food") {
-    message = "Cơm của Top One";
+}: MenuItemListProps) => {
+  interface Messages {
+    [key: string]: string | { [key: string]: string };
   }
+
+  const messages: Messages = {
+    drink: {
+      tea: "Trà trái cây của Top One",
+      yogurt: "Sữa chua của Top One",
+      juice: "Nước ép, đá xay của Top One",
+      "milk-tea": "Trà sữa của Top One",
+      other: "Các thức uống khác của Top One",
+      default: "Cà phê, trà sữa của Top One",
+    },
+    noodle: "Mỳ cay của Top One",
+    food: "Cơm của Top One",
+  };
+
+  const message =
+    type === "drink"
+      ? (messages[type] as { [key: string]: string })[subType] ||
+        (messages[type] as { [key: string]: string }).default
+      : messages[type];
+
+  const filteredData =
+    subType === "all"
+      ? data
+      : subType
+      ? data.filter((item) => item.sub_type === type + "_" + subType)
+      : data;
 
   return (
     <div>
       <div className="flex flex-col gap-4">
-        {type !== "all" && <h1>{message}</h1>}
-        {type === "all" && <h1 className="text-red-300">{title}</h1>}
+        {type !== "all" && (
+          <h1 className="text-2xl font-bold">{message.toString()}</h1>
+        )}
+        {type === "all" && <h1 className="text-2xl font-bold">{title}</h1>}
         <div className="grid grid-cols-3 gap-5">
-          {!subType && data.map((item) => <MenuItem data={item} />)}
-          {subType &&
-            data
-              .filter((item) => item.sub_type === type + "_" + subType)
-              .map((item) => <MenuItem data={item} />)}
-          {subType === "all" && data.map((item) => <MenuItem data={item} />)}
+          {filteredData.map((item) => (
+            <MenuItem data={item} />
+          ))}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default MenuItemList;
--- src/helper/const.ts
+++ src/helper/const.ts
@@ -74,7 +74,14 @@ export const menuOptionsWithSubTypes = [
 export const menuShortOption = [
   { value: "name-asc", label: "Theo tên (A-Z)" },
   { value: "name-desc", label: "Theo tên (Z-A)" },
-  { value: "price-asc", label: "Theo giá (Thấp trước)" },
-  { value: "price-desc", label: "Theo giá (Cao trước)" },
+  { value: "price-asc", label: "Theo giá: thấp đến cao" },
+  { value: "price-desc", label: "Theo giá: cao đến thấp" },
   { value: "id-desc", label: "Theo ngày tạo (gần nhất trước)" },
+];
+export const menuShortOption1 = [
+  { value: "id-desc", label: "Mới nhất" },
+  { value: "price-asc", label: "Theo giá: thấp đến cao" },
+  { value: "price-desc", label: "Theo giá: cao đến thấp" },
+  { value: "name-asc", label: "Theo tên: (A-Z)" },
+  { value: "name-desc", label: "Theo tên: (Z-A)" },
 ];
