d1fdbb07f2803220337bad81fae46de16d17ca7f
dngocson <dngocson12@gmail.com>
2023-08-28 13:55:43 +0700

    improve scrooling
    improve scrooling

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { Provider } from "react-redux";
 import store from "./features/redux/store.ts";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorBoundayElement from "./ui/ErrorBoundayElement.tsx";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ErrorBoundary
--- src/ui/AppLayout.tsx
+++ src/ui/AppLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
+import ScrollToTop from "./ScroolToTop";
 function AppLayout() {
   return (
     <div className="bg-[#ffffff]">
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
