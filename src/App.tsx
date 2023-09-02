b23474d8f5a48cc3abb64d7ecc710a76af99c838
dngocson <dngocson12@gmail.com>
2023-09-02 14:43:42 +0700

    added lazy loading
    added lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,33 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
-
+import { lazy, Suspense } from "react";
+import ProtectedRoutes from "./ui/ProtectedRoutes";
+import Spinner from "./ui/Spinner";
 import AppLayout from "./ui/AppLayout";
 import ErrorPage from "./pages/ErrorPage";
 import Main from "./pages/Main";
-import Order from "./pages/Order";
-import OrderDetail from "./pages/OrderDetail";
-import Settings from "./pages/Settings";
-import Menu from "./pages/Menu";
-import Contact from "./pages/Contact";
-import Login from "./pages/Login";
-import About from "./pages/About";
-import ProductDetail from "./pages/ProductDetail";
-import Admin from "./pages/Admin";
-import ProtectedRoutes from "./ui/ProtectedRoutes";
-import FindOrder from "./pages/FindOrder";
+// import OrderDetail from "./pages/OrderDetail";
+// import Menu from "./pages/Menu";
+// import Order from "./pages/Order";
+// import Login from "./pages/Login";
+// import About from "./pages/About";
+// import Settings from "./pages/Settings";
+// import ProductDetail from "./pages/ProductDetail";
+// import Contact from "./pages/Contact";
+// import Admin from "./pages/Admin";
+// import FindOrder from "./pages/FindOrder";
+
+const Order = lazy(() => import("./pages/Order"));
+const Login = lazy(() => import("./pages/Login"));
+const About = lazy(() => import("./pages/About"));
+const Settings = lazy(() => import("./pages/Settings"));
+const Contact = lazy(() => import("./pages/Contact"));
+const Admin = lazy(() => import("./pages/Admin"));
+const ProductDetail = lazy(() => import("./pages/ProductDetail"));
+const FindOrder = lazy(() => import("./pages/FindOrder"));
+const Menu = lazy(() => import("./pages/Menu"));
+const OrderDetail = lazy(() => import("./pages/OrderDetail"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -32,18 +44,69 @@ const App = () => {
       errorElement: <ErrorPage />,
       children: [
         { path: "/", element: <Main /> },
-        { path: "/about", element: <About /> },
-        { path: "/menu", element: <Menu /> },
-        { path: "/menu/:productId", element: <ProductDetail /> },
-        { path: "/login", element: <Login /> },
-        { path: "/contact", element: <Contact /> },
-        { path: "/order/:orderId", element: <OrderDetail /> },
-        { path: "/find", element: <FindOrder /> },
+        {
+          path: "/about",
+          element: (
+            <Suspense fallback={<Spinner />}>
+              <About />
+            </Suspense>
+          ),
+        },
+        {
+          path: "/menu",
+          element: (
+            <Suspense fallback={<Spinner />}>
+              <Menu />
+            </Suspense>
+          ),
+        },
+        {
+          path: "/menu/:productId",
+          element: (
+            <Suspense fallback={<Spinner />}>
+              <ProductDetail />
+            </Suspense>
+          ),
+        },
+        {
+          path: "/login",
+          element: (
+            <Suspense fallback={<Spinner />}>
+              <Login />
+            </Suspense>
+          ),
+        },
+        {
+          path: "/contact",
+          element: (
+            <Suspense fallback={<Spinner />}>
+              <Contact />
+            </Suspense>
+          ),
+        },
+        {
+          path: "/order/:orderId",
+          element: (
+            <Suspense fallback={<Spinner />}>
+              <OrderDetail />
+            </Suspense>
+          ),
+        },
+        {
+          path: "/find",
+          element: (
+            <Suspense fallback={<Spinner />}>
+              <FindOrder />
+            </Suspense>
+          ),
+        },
         {
           path: "/admin",
           element: (
             <ProtectedRoutes>
-              <Admin />
+              <Suspense fallback={<Spinner />}>
+                <Admin />
+              </Suspense>
             </ProtectedRoutes>
           ),
         },
@@ -51,7 +114,9 @@ const App = () => {
           path: "/admin/settings",
           element: (
             <ProtectedRoutes>
-              <Settings />
+              <Suspense fallback={<Spinner />}>
+                <Settings />
+              </Suspense>
             </ProtectedRoutes>
           ),
         },
@@ -59,7 +124,9 @@ const App = () => {
           path: "/admin/order",
           element: (
             <ProtectedRoutes>
-              <Order />
+              <Suspense fallback={<Spinner />}>
+                <Order />
+              </Suspense>
             </ProtectedRoutes>
           ),
         },
